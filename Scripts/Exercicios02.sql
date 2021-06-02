9f122e38495e2825295404cefb613a04d7cd72fb
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2021-06-02 13:50:05 -0300

    Exercício 02
    Concluído.

--- Scripts/Exercicios02.sql
+++ Scripts/Exercicios02.sql
@@ -5,9 +5,29 @@ SELECT * FROM CARRO
 SELECT * FROM PECAS
 SELECT * FROM SERVICO
 
-SELECT CLIENTE.telefone, CARRO.marca, CARRO.cor
-FROM Concessionaria.dbo.CLIENTE
-INNER JOIN Concessionaria.dbo.CARRO
-ON CARRO.
-WHERE Carro.modelo LIKE 'Ka'
-AND
+SELECT cli.telefone, c.marca, c.cor
+FROM Concessionaria.dbo.CARRO AS c
+INNER JOIN Concessionaria.dbo.CLIENTE AS cli
+ON c.placa = cli.carro_placa
+AND c.modelo LIKE 'Ka'
+AND c.cor LIKE 'Azul'
+
+SELECT cli.logradouro, cli.n, cli.bairro, s.data_
+FROM Concessionaria.dbo.SERVICO AS s
+INNER JOIN Concessionaria.dbo.CLIENTE AS cli
+ON s.carro = cli.carro_placa
+AND s.data_ LIKE '%2009%'
+
+SELECT c.placa
+FROM Concessionaria.dbo.CARRO AS c
+WHERE (c.ano < 2001)
+
+SELECT c.marca, c.modelo, c.cor
+FROM Concessionaria.dbo.CARRO AS c
+INNER JOIN Concessionaria.dbo.CARRO as cc
+ON c.placa = cc.placa
+WHERE (c.ano > 2005)
+
+SELECT p.codigo, p.nome, p.valor
+FROM Concessionaria.dbo.PECAS AS p
+WHERE (p.valor < 80)
