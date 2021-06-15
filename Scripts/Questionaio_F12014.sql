ab63ac816963517ec823408826050a9c6fc1686d
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2021-06-15 18:01:52 -0300

    Update Questionaio_F12014.sql
    Exercício em progresso.

--- Scripts/Questionaio_F12014.sql
+++ Scripts/Questionaio_F12014.sql
@@ -25,10 +25,23 @@ ON e.codigo = t.numero_piloto
 --Consultar o numero do piloto e o nome do piloto dos pilotos inscritos que não participaram
 --de nenhuma corrida nesse campeonato
 
+SELECT p.numero, p.nome
+FROM piloto p LEFT OUTER JOIN temporada t
+ON t.numero_piloto = p.numero
+WHERE t.numero_piloto IS NULL
+
 --Consultar o numero do piloto, o nome do piloto, o nome da equipe (em uma coluna chamada equipe),
 --a posição e a quanqtidade de vezes que o piloto chegou nessa posição (coluna chamada qtd),
 --do piloto que tem Hamilton no nome.
 --Se a posição for 0 (Escrever DNF), se for 1 (Escrever 1o.), se for 2 (Escrever 2o.), e assim por diante.
+
+SELECT p.numero, p.nome, e.nome AS equipe, t.posicao, COUNT (t.posicao) AS qtd
+FROM equipe e, piloto p INNER JOIN temporada t
+ON t.numero_piloto = p.numero
+WHERE p.nome LIKE '%Hamilton%'
+-- Falta case when
+GROUP BY p.numero,p.nome, e.nome, t.posicao
+
 
 --Consultar, o código da corrida, o pais, e a quantidade de abandonos 
 --(quantidade de pilotos cuja posicao = 0) em uma coluna chamada qtd_abandonos
